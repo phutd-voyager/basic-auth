269efb4aad1ca1ad5724da61e1af057649376eb4
Phu Tran Dinh <phutd@voyager-hcm.com>
2024-05-10 14:35:14 +0700

    Initial code

--- tests/BasicAuthServiceTest.php
+++ tests/BasicAuthServiceTest.php
@@ -5,27 +5,46 @@ namespace VoyagerInc\BasicAuth\Tests;
 class BasicAuthServiceTest extends BaseTest
 {
     #[test]
-    public function test_can_authenticate_user()
+    public function test_get_username()
     {
-        $username = 'admin';
-        $password = 'password';
+        config(['basic_auth.username' => 'custom_username']);
+        $this->assertEquals('custom_username', $this->basicAuthService->getUsername());
 
-        $this->basicAuthService->authenticate($username, $password);
-
-        $this->assertTrue($this->basicAuthService->isAuthenticated());
-
-        $this->assertEquals($username, $this->basicAuthService->getUsername());
-        $this->assertEquals($password, $this->basicAuthService->getPassword());
+        config(['basic_auth.username' => '']);
+        $this->assertEquals('admin', $this->basicAuthService->getUsername());
     }
 
     #[test]
-    public function test_cant_authenticate_user()
+    public function test_get_password()
     {
-        $username = 'admin1';
-        $password = 'password';
+        config(['basic_auth.password' => 'custom_password']);
+        $this->assertEquals('custom_password', $this->basicAuthService->getPassword());
 
-        $this->basicAuthService->authenticate($username, $password);
+        config(['basic_auth.password' => '']);
+        $this->assertEquals('password', $this->basicAuthService->getPassword());
+    }
 
+    #[test]
+    public function test_authenticate()
+    {
+        $this->basicAuthService->authenticate('admin', 'password');
+        $this->assertTrue($this->basicAuthService->isAuthenticated());
+
+        $this->basicAuthService->authenticate('admin', 'wrong_password');
+        $this->assertFalse($this->basicAuthService->isAuthenticated());
+
+        config(['basic_auth.enabled' => false]);
+        $this->basicAuthService->authenticate('admin', 'password');
+        $this->assertFalse($this->basicAuthService->isAuthenticated());
+    }
+
+    #[test]
+    public function test_is_authenticated()
+    {
+        $this->basicAuthService->authenticate('admin', 'password');
+        $this->assertTrue($this->basicAuthService->isAuthenticated());
+
+        $this->basicAuthService->authenticate('admin', 'wrong_password');
         $this->assertFalse($this->basicAuthService->isAuthenticated());
     }
 }
